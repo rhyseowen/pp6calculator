dd89c0fcec6037821479c0254316b1140d94c780
Rhys Owen <rhyseowen@users.noreply.github.com>
2014-03-10 17:46:58 +0000

    made menu logie easier to follow by moving week one opperations to their on functions

--- PP6Menu/weekOneMenu.cpp
+++ PP6Menu/weekOneMenu.cpp
@@ -29,70 +29,42 @@ int weekOneMenu()
 		if (opperation == "+")
 		{
 			//addition
-			std::cout << "a + b = ?" << std::endl;
-			std::vector<double> number = readInNumbers(2);
-			std::cout << number[0] << " + " << number[1] << " = " << addition(number[0],number[1]) <<std::endl;  
+			err = wk1_addition();
+  
 		}else if (opperation == "-")
 		{
 			//subtraction
-			std::cout << "a - b = ?" << std::endl;
-			std::vector<double> number = readInNumbers(2);
-			std::cout << number[0] << " - " << number[1] << " = " << subtraction(number[0],number[1]) <<std::endl; 
+			err = wk1_subtraction();
 		}else if (opperation == "/")
 		{
 			//division
-			std::cout << "a / b = ?" << std::endl;
-			std::vector<double> number = readInNumbers(2);
-			if (number[1] == 0)
-			{
-				std::cout << "Cannot divide by 0!" << std::endl;
-				err = 3;
-				continue;
-			}
-			std::cout << number[0] <<  " / " << number[1] << " = " << divide(number[0],number[1]) <<std::endl; 	
+			err = wk1_division();
+
 		}else if (opperation == "*")
 		{
 			//multiplication
-			std::cout << "a * b = ?" <<std::endl;
-			std::vector<double> number = readInNumbers(2);
-			std::cout << number[0] << " * " << number[1] << " = " << multiply(number[0],number[1]) <<std::endl;
+			err = wk1_multiplication();
+
 		}else if (opperation == "solveAxis")
 		{
-			std::cout << " y = ax + b: find x intercept" << std::endl;
-			std::vector<double> number = readInNumbers(2);
-			std::cout << " y = " << number[0] << "x + " << number[1] << ": x intercept = " << solveXaxis(number[0], number[1]) << std::endl;
+			err = wk1_solveAxis();
+
 		}else if (opperation == "solveQuadratic")
 		{
-			std::cout << "0 = ax^2 + bx +c: solve for x" << std::endl;
-			std::vector<double> number = readInNumbers(3);
+			err = wk1_solveQuadratic();
 
-			double solution1 = 0;
-			double solution2 = 0;
-
-			if (solveQuadratic(number[0],number[1],number[2],solution1,solution2))
-			{
-				std::cout << "0 = " <<  number[0] << "x^2 + " << number[1] << "x + " << number[2] << " : x = " << solution1 << " or " << solution2 << std::endl;
-			}else{
-				std::cout << "No real roots try again!" << std::endl;
-				err = 4;
-			}
 		}else if (opperation == "3VectorLength")
 		{
-			std::cout << "v = ai + bj + ck : calculate |v|" << std::endl;
-			std::vector<double> number = readInNumbers(3);
-			std::cout << "v = " << number[0] << "i + " << number[1] << "j + " << number[2] << "k : |v| = " << vectorLength3(number[0], number[1], number[2]) << std::endl; 
+			err = wk1_3VectorLength();
+
 		}else if (opperation == "4VectorLength")
 		{
-			std::cout << "A = (a,b,c,d) : find |A|" << std::endl;
-			std::vector<double> number = readInNumbers(4);
-			std::cout << "A = (" << number[0] << "," << number[1] << "," << number[2] << "," << number[3] << ") : |A| = " << vectorDotProductSqrt4(number[0],number[1],number[2],number[3],number[0],number[1],number[2],number[3]) <<std::endl;
+			err = wk1_4VectorLength();
+
 		}else if (opperation == "invarientMass")
 		{
-			std::cout << "m^2 = A1*A2 = (a,b,c,d)*(e,f,g,h) : find |m|" << std::endl;
-			std::vector<double> number = readInNumbers(8);
-			std::cout << "A1 = (" << number[0] << "," << number[1] << "," << number[2] << "," << number[3] << ")" << std::endl;
-			std::cout << "A2 = (" << number[4] << "," << number[5] << "," << number[6] << "," << number[7] << ")" << std::endl;
-			std::cout << "m = " << vectorDotProductSqrt4(number[0],number[1],number[2],number[3],number[4],number[5],number[6],number[7]) << std::endl;
+			err = wk1_invarientMass();
+
 		}else if (opperation == "swap")
 		{
 			std::cout << " swap a and b" << std::endl;
@@ -163,4 +135,94 @@ void weekOnePrintHelp()
 	std::cout << "sort : sort 8 numbers" << std::endl;
 	std::cout << "vectorSort : sort 8 numbers (using a vector instead of an array)" << std::endl;
 	std::cout << "quit : exit the calculator" << std::endl;
+}
+
+int wk1_addition()
+{
+	std::cout << "a + b = ?" << std::endl;
+	std::vector<double> number = readInNumbers(2);
+	std::cout << number[0] << " + " << number[1] << " = " << addition(number[0],number[1]) <<std::endl;
+
+	return 0;
+}
+int wk1_subtraction()
+{
+	std::cout << "a - b = ?" << std::endl;
+	std::vector<double> number = readInNumbers(2);
+	std::cout << number[0] << " - " << number[1] << " = " << subtraction(number[0],number[1]) <<std::endl; 
+	return 0;
+}
+int wk1_division()
+{
+	std::cout << "a / b = ?" << std::endl;
+	std::vector<double> number = readInNumbers(2);
+	if (number[1] == 0)
+	{
+		std::cout << "Cannot divide by 0!" << std::endl;
+		return 3;
+		
+	}
+	std::cout << number[0] <<  " / " << number[1] << " = " << divide(number[0],number[1]) <<std::endl; 	
+
+	return 0;
+}
+int wk1_multiplication()
+{
+	std::cout << "a * b = ?" <<std::endl;
+	std::vector<double> number = readInNumbers(2);
+	std::cout << number[0] << " * " << number[1] << " = " << multiply(number[0],number[1]) <<std::endl;
+
+	return 0;
+}
+int wk1_solveAxis()
+{
+	std::cout << " y = ax + b: find x intercept" << std::endl;
+	std::vector<double> number = readInNumbers(2);
+	std::cout << " y = " << number[0] << "x + " << number[1] << ": x intercept = " << solveXaxis(number[0], number[1]) << std::endl;
+
+	return 0;
+}
+int wk1_solveQuadratic()
+{
+	std::cout << "0 = ax^2 + bx +c: solve for x" << std::endl;
+	std::vector<double> number = readInNumbers(3);
+
+	double solution1 = 0;
+	double solution2 = 0;
+
+	if (solveQuadratic(number[0],number[1],number[2],solution1,solution2))
+	{
+		std::cout << "0 = " <<  number[0] << "x^2 + " << number[1] << "x + " << number[2] << " : x = " << solution1 << " or " << solution2 << std::endl;
+	}else{
+		std::cout << "No real roots try again!" << std::endl;
+		return 4;
+	}
+
+	return 0;
+}
+int wk1_3VectorLength()
+{
+	std::cout << "v = ai + bj + ck : calculate |v|" << std::endl;
+	std::vector<double> number = readInNumbers(3);
+	std::cout << "v = " << number[0] << "i + " << number[1] << "j + " << number[2] << "k : |v| = " << vectorLength3(number[0], number[1], number[2]) << std::endl; 
+
+	return 0;
+}
+int wk1_4VectorLength()
+{
+
+	std::cout << "A = (a,b,c,d) : find |A|" << std::endl;
+	std::vector<double> number = readInNumbers(4);
+	std::cout << "A = (" << number[0] << "," << number[1] << "," << number[2] << "," << number[3] << ") : |A| = " << vectorDotProductSqrt4(number[0],number[1],number[2],number[3],number[0],number[1],number[2],number[3]) <<std::endl;
+	return 0;
+}
+int wk1_invarientMass()
+{
+	std::cout << "m^2 = A1*A2 = (a,b,c,d)*(e,f,g,h) : find |m|" << std::endl;
+	std::vector<double> number = readInNumbers(8);
+	std::cout << "A1 = (" << number[0] << "," << number[1] << "," << number[2] << "," << number[3] << ")" << std::endl;
+	std::cout << "A2 = (" << number[4] << "," << number[5] << "," << number[6] << "," << number[7] << ")" << std::endl;
+	std::cout << "m = " << vectorDotProductSqrt4(number[0],number[1],number[2],number[3],number[4],number[5],number[6],number[7]) << std::endl;
+
+	return 0;
 }
--- PP6Menu/weekOneMenu.hpp
+++ PP6Menu/weekOneMenu.hpp
@@ -4,4 +4,14 @@
 int weekOneMenu();
 void weekOnePrintHelp();
 
+int wk1_addition();
+int wk1_subtraction();
+int wk1_division();
+int wk1_multiplication();
+int wk1_solveAxis();
+int wk1_solveQuadratic();
+int wk1_3VectorLength();
+int wk1_4VectorLength();
+int wk1_invarientMass();
+
 #endif

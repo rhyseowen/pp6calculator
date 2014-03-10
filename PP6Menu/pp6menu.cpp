0b91d2c86d09655ab478781fd90e958dda201d7e
Rhys Owen <rhyseowen@users.noreply.github.com>
2014-03-10 22:28:38 +0000

    Update Comments

--- PP6Menu/pp6menu.cpp
+++ PP6Menu/pp6menu.cpp
@@ -1,8 +1,17 @@
 #include "pp6menu.hpp"
 
+/*
+Nominal "superclass" for menus for helpfull functions which arent "math"
+*/
+
 
 std::vector<double> readInNumbers(int numbers)
 {
+	/*
+		Function to read in a variable number of doubles from std::cin. Maximum number is 8 
+		dictated by the length of the charlist
+
+	*/
 	std::vector<double> in_numbers;
 
 	for (int i = 0; i < numbers; ++i)
@@ -14,6 +23,7 @@ std::vector<double> readInNumbers(int numbers)
 
 		if (!std::cin)
 		{
+			//if cin is broken flush and jump back an itteration
 			std::cout << "Number not recognised" << std::endl;
 			std::cin.clear();
 			std::cin.ignore(INT_MAX, '\n');

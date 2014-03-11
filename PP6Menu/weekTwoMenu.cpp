3d1030d0acb2a5507e77d86c85305bd914983b56
Rhys Owen <rhyseowen@users.noreply.github.com>
2014-03-11 11:29:23 +0000

    Add read in for muon momentum

--- PP6Menu/weekTwoMenu.cpp
+++ PP6Menu/weekTwoMenu.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <climits>
 #include <vector>
+#include <cmath>
 
 #include "weekTwoMenu.hpp"
 #include "PP6Math.hpp"
@@ -152,6 +153,72 @@ int wk2_readInMuons()
 	getNumberOfMuons("observedparticles.dat",number_mu_p,number_mu_n);	
 
 	std::cout << "No. mu+ = " << number_mu_p << " mu- = " << number_mu_n << std::endl;
+
+	if ((number_mu_p > 128) || (number_mu_n > 128))
+	{
+		std::cout << "Too many muons" << std::endl;
+		return 4;
+	}
+
+	//set up arrays to hold muon information
+
+	double mu_p_px[128] = {0};
+	double mu_p_py[128] = {0};
+	double mu_p_pz[128] = {0};
+	double mu_p_E[128] = {0};
+
+	double mu_n_px[128] = {0};
+	double mu_n_py[128] = {0};
+	double mu_n_pz[128] = {0};
+	double mu_n_E[128] = {0};
+
+	getMuonInformation("observedparticles.dat", 105.7, mu_p_px, mu_p_py, mu_p_pz, mu_p_E, mu_n_px, mu_n_py, mu_n_pz, mu_n_E);
+
+	for (int i = 0; i < 128; ++i)
+	{
+		std::cout << mu_p_px[i] << std::endl;
+	}
+
+	return 0;
+}
+
+int getMuonInformation(std::string filename, double muonMass, double *mu_p_px, double *mu_p_py, double *mu_p_pz, double *mu_p_E, double *mu_n_px, double *mu_n_py, double *mu_n_pz, double *mu_n_E)
+{
+	int number_mu_p = 0;
+	int number_mu_n = 0;
+	//use file reader to open up file
+	FileReader f(filename);
+	// Only process if the file is open/valid
+	if (f.isValid()) {
+  		// Loop until out of lines
+		while (f.nextLine()) {
+
+    		// retrieve field 2 as a string (starting at 1)
+			std::string particle = f.getFieldAsString(2);
+
+			if (particle == "mu+")
+			{
+				mu_p_px[number_mu_p] = f.getFieldAsDouble(3);
+				mu_p_py[number_mu_p] = f.getFieldAsDouble(4);
+				mu_p_pz[number_mu_p] = f.getFieldAsDouble(5);
+
+				mu_p_E[number_mu_p] = sqrt(muonMass*muonMass + mu_p_px[number_mu_p]*mu_p_px[number_mu_p] + mu_p_py[number_mu_p]*mu_p_py[number_mu_p] + mu_p_pz[number_mu_p]*mu_p_pz[number_mu_p]);
+				++number_mu_p;
+			}else if (particle == "mu-")
+			{
+				mu_n_px[number_mu_n] = f.getFieldAsDouble(3);
+				mu_n_py[number_mu_n] = f.getFieldAsDouble(4);
+				mu_n_pz[number_mu_n] = f.getFieldAsDouble(5);
+
+				mu_n_E[number_mu_n] = sqrt(muonMass*muonMass + mu_n_px[number_mu_n]*mu_n_px[number_mu_n] + mu_n_py[number_mu_n]*mu_n_py[number_mu_n] + mu_n_pz[number_mu_n]*mu_n_pz[number_mu_n]);
+
+				++number_mu_n;
+			}
+
+    		// Check that input is o.k.
+			if (f.inputFailed()) break;
+		}
+	}
 
 	return 0;
 }

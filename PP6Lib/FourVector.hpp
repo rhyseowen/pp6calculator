8b6c6766d8ca7e043ebad41f67e6df9fe85ea865
Rhys Owen <rhyseowen@users.noreply.github.com>
2014-03-18 18:25:45 +0000

    Update FourVector header
    * Add more doxy comments
    * Make operators friend functions

--- PP6Lib/FourVector.hpp
+++ PP6Lib/FourVector.hpp
@@ -45,31 +45,67 @@ class FourVector{
 
 		//getters and setters
 
+		/**
+		 * @brief get a0
+		 */
 		double getA0() const;
 
+		/**
+		 * @brief get a1
+		 */
 		double getA1() const;
 
+		/**
+		 * @brief get a2
+		 */
 		double getA2() const;
 
+		/**
+		 * @brief get a3
+		 */
 		double getA3() const;
 
+		/**
+		 * @brief set a0 component
+		 */
 		void setA0(double a0_in);
 
+		/**
+		 * @brief set a1 component
+		 */
 		void setA1(double a1_in);
 
+		/**
+		 * @brief set a2 component
+		 */
 		void setA2(double a2_in);
 
+		/**
+		 * @brief set a3 component
+		 */
 		void setA3(double a3_in);
 
+		/**
+		 * @brief Get the four vector interval
+		 * @details Returns the interval of the four vector ie \f$A_{\mu}A^{\mu} = \text{interval}\f$
+		 * @return interval
+		 */
 		double getInterval() const; 
 
+		/**
+		 * @brief Add another FourVector component wise
+		 * 
+		 * @param rhs Other FourVector
+		 */
 		FourVector& operator+=(const FourVector& rhs);
 
 		FourVector& operator-=(const FourVector& rhs);
 
 		FourVector& operator=(const FourVector& rhs);
 
+		friend FourVector operator+(const FourVector& lhs, const FourVector& rhs);
 
+		friend FourVector operator-(const FourVector& lhs, const FourVector& rhs);
 
 
 	private:
@@ -89,9 +125,7 @@ class FourVector{
 
 };
 
-FourVector operator+(const FourVector& lhs, const FourVector& rhs);
 
-FourVector operator-(const FourVector& lhs, const FourVector& rhs);
 
 
 

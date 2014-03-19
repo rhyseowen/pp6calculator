089e06a7d6deec4a83f55033f7d3eedac791b6c3
Rhys Owen <rhyseowen@users.noreply.github.com>
2014-03-19 17:01:40 +0000

    Add details to ThreeVector header

--- PP6Lib/ThreeVector.hpp
+++ PP6Lib/ThreeVector.hpp
@@ -2,9 +2,66 @@
 #define THREEVECTOR_HH 
 
 class ThreeVector{
+	private:
+		double x_;
+		double y_;
+		double z_;
 
+		double lengthSquared_;
 
+	public:
+		ThreeVector();
+		ThreeVector(double x, double y, double z);
+		ThreeVector(const ThreeVector& other);
 
+		void dotProduct(const ThreeVector& rhs);
+		void crossProduct(const ThreeVector& rhs);
+
+		double getX() const;
+		double getY() const;
+		double getZ() const;
+
+		void setX(double x);
+		void setY(double y);
+		void setZ(double z);
+
+		ThreeVector& operator+=(const ThreeVector& rhs);
+		ThreeVector& operator-=(const ThreeVector& rhs);
+		ThreeVector& operator=(const ThreeVector& rhs);
+
+		ThreeVector& operator*=(const int& rhs);
+		ThreeVector& operator*=(const double& rhs);
+		ThreeVector& operator*=(const float& rhs);
+
+		ThreeVector& operator/=(const int& rhs);
+		ThreeVector& operator/=(const double& rhs);
+		ThreeVector& operator/=(const float& rhs);
+
+		bool operator==(const ThreeVector& rhs);
+		bool operator!=(const ThreeVector& rhs);
+
+		friend ThreeVector operator+(const ThreeVector& lhs, const ThreeVector& rhs);
+		friend ThreeVector operator-(const ThreeVector& lhs, const ThreeVector& rhs);
+
+		friend ThreeVector operator*(const ThreeVector& lhs, const int& rhs);
+		friend ThreeVector operator*(const int& lhs, const ThreeVector& rhs);
+
+		friend ThreeVector operator*(const ThreeVector& lhs, const double& rhs);
+		friend ThreeVector operator*(const double& lhs, const ThreeVector& rhs);
+
+		friend ThreeVector operator*(const ThreeVector& lhs, const float& rhs);
+		friend ThreeVector operator*(const float& lhs, const ThreeVector& rhs);
+
+		friend ThreeVector operator/(const ThreeVector& lhs, const int& rhs);
+		friend ThreeVector operator/(const int& lhs, const ThreeVector& rhs);
+
+		friend ThreeVector operator/(const ThreeVector& lhs, const double& rhs);
+		friend ThreeVector operator/(const double& lhs, const ThreeVector& rhs);
+
+		friend ThreeVector operator/(const ThreeVector& lhs, const float& rhs);
+		friend ThreeVector operator/(const float& lhs, const ThreeVector& rhs);
+
+		//@todo stream operators
 
 };
 

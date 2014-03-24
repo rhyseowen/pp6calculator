e723cc65c86cc6fd2c8274f0711717567afd4b7a
Rhys Owen <rhyseowen@users.noreply.github.com>
2014-03-24 15:52:28 +0000

    Update after last day 4 exercise

--- search/classes_1.js
+++ search/classes_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['particle',['Particle',['../classParticle.html',1,'']]]
+  ['particle',['Particle',['../classParticle.html',1,'']]],
+  ['particleinfo',['ParticleInfo',['../classParticleInfo.html',1,'']]]
 ];
